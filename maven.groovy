8229425a32b29b32b905bd87111db8ceecd6f64f
Eduardo Cabrera <lalo@eof.cl>
2022-01-28 21:20:59 -0300

    con parametros

---
+++ maven.groovy
@@ -0,0 +1,65 @@
+def call(){
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+def call(){
+    stage("Paso 1: Compilar"){
+        sh "mvn clean compile -e"
+    }
+    stage("Paso 2: Testear"){
+
+        sh "mvn clean test -e"
+    }
+    stage("Paso 3: Build .Jar"){
+        sh "mvn clean package -e"
+    }
+
+    stage("Paso 4: Sonar - Análisis Estático"){
+        sh "echo 'Análisis Estático!'"
+        withSonarQubeEnv('sonarqube') {
+            sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+        }
+    }
+
+    stage("Paso 5: Curl Springboot Gradle sleep 20"){
+        sh "timeout -k 30 mvn spring-boot:run &"
+        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        sh "sleep 10"
+    }
+
+    stage("Paso 6: Subir Nexus"){
+        nexusPublisher nexusInstanceId: 'nexus',
+        nexusRepositoryId: 'devops-usach-nexus',
+        packages: [
+            [$class: 'MavenPackage',
+                mavenAssetList: [
+                    [classifier: '',
+                    extension: 'jar',
+                    filePath: 'build/DevOpsUsach2020-0.0.1.jar'
+                ]
+            ],
+                mavenCoordinate: [
+                    artifactId: 'DevOpsUsach2020',
+                    groupId: 'com.devopsusach2020',
+                    packaging: 'jar',
+                    version: '0.0.1'
+                ]
+            ]
+        ]
+    }
+    stage("Paso 7: Descargar Nexus"){
+        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus3:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+    }
+    stage("Paso 8: Levantar Artefacto Jar"){
+        sh 'nohup java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
+    }
+    stage("Paso 9: Testear Artefacto - Dormir(Esperar 20sg) "){
+        sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    }
+}
+return this;
+}
+
+return this;
